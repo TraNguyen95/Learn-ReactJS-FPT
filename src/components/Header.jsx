004e29c96b2af606ca09a606ef66f95899520b2a
Trà Channels <nguyenhuongtra24@gmail.com>
2021-09-22 17:12:54 +0700

    reponsive header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 import logo from "../assets/images/Logo-2.png";
 import { Link, useLocation } from "react-router-dom";
@@ -25,8 +25,30 @@ const mainNav = [
 const Header = () => {
   const { pathname } = useLocation();
   const activeNav = mainNav.findIndex((e) => e.path === pathname);
+
+  const headerRef = useRef(null);
+
+  const menuLeft = useRef(null);
+  const menuToggle = () => menuLeft.current.classList.toggle("active");
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (
+        document.body.scrollTop > 80 ||
+        document.documentElement.scrollTop > 80
+      ) {
+        headerRef.current.classList.add("shrink");
+      } else {
+        headerRef.current.classList.remove("shrink");
+      }
+    });
+    return () => {
+      window.removeEventListener("scroll", null);
+    };
+  }, []);
+
   return (
-    <div className="header">
+    <div className="header" ref={headerRef}>
       <div className="container">
         <div className="header__logo">
           <Link to="/">
@@ -34,11 +56,11 @@ const Header = () => {
           </Link>
         </div>
         <div className="header__menu">
-          <div className="header__menu__mobile-toggle">
+          <div className="header__menu__mobile-toggle" onClick={menuToggle}>
             <i className="bx bx-menu"></i>
           </div>
-          <div className="header__menu__left">
-            <div className="header__menu__left__close">
+          <div className="header__menu__left" ref={menuLeft}>
+            <div className="header__menu__left__close" onClick={menuToggle}>
               <i className="bx bx-chevron-left"></i>
             </div>
             {mainNav.map((item, index) => (
@@ -47,6 +69,7 @@ const Header = () => {
                 className={`header__menu__item header__menu__left__item ${
                   index === activeNav ? "active" : ""
                 }`}
+                onClick={menuToggle}
               >
                 <Link to={item.path}>
                   <span>{item.display}</span>
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Layout from "./components/Layout";
 import reportWebVitals from "./reportWebVitals";
 
 import "./assets/boxicons-2.0.7/css/boxicons.min.css";
 import "./sass/index.scss";
+
+import Layout from "./components/Layout";
+
 ReactDOM.render(
   <React.StrictMode>
     <Layout />
