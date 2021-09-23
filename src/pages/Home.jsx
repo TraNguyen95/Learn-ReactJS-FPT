a16a44f90b2c3911e1a0dbde87fdb6a17c6129e5
Trà Channels <nguyenhuongtra24@gmail.com>
2021-09-23 16:23:16 +0700

    add hero-slider and footer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import Helmet from "../components/Helmet";
+import HeroSlider from "../components/HeroSlider";
+
+import heroSliderData from "../assets/fake-data/hero-slider";
+import policy from "../assets/fake-data/policy";
+import productData from "../assets/fake-data/products";
+
+import banner from "../assets/images/banner.png";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <Helmet title="Trang chủ">
+      {/* hero slider */}
+      <HeroSlider
+        data={heroSliderData}
+        control={true}
+        auto={false}
+        timeOut={5000}
+      />
+      {/* end hero slider */}
+    </Helmet>
+  );
 };
 
 export default Home;
